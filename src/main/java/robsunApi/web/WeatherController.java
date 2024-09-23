129db3f424745b272fc2d4bac0d16128ba9c1e50
Robsunnn <robertospasors@gmail.com>
2024-09-23 14:12:09 +0300

    Fixing problem with receiving the city param

--- src/main/java/robsunApi/web/WeatherController.java
+++ src/main/java/robsunApi/web/WeatherController.java
@@ -2,7 +2,7 @@ package robsunApi.web;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import static robsunApi.service.WeatherService.getWeatherInfo;
@@ -10,8 +10,8 @@ import static robsunApi.service.WeatherService.getWeatherInfo;
 @RestController
 public class WeatherController {
 
-    @GetMapping("/weather")
-    public ResponseEntity<String> getWeather(@RequestParam String city) {
+    @GetMapping("/weather/{city}")
+    public ResponseEntity<String> getWeather(@PathVariable("city") String city) {
         // Call the getWeatherInfo method from the WeatherService class
         String weatherData = getWeatherInfo(city);
 
