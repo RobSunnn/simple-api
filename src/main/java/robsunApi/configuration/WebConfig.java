9a8d855ce1272f8df04be01e6a244e0f74cacbe1
Robsunnn <robertospasors@gmail.com>
2024-09-23 14:34:43 +0300

    Added configuration file for allowing post method

---
+++ src/main/java/robsunApi/configuration/WebConfig.java
@@ -0,0 +1,16 @@
+package robsunApi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/weather")  // Allow CORS on the /weather endpoint
+                .allowedOrigins("https://robsunnn.github.io/")
+                .allowedMethods("POST");  // Allow POST requests
+    }
+}
