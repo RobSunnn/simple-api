ce238d3ed4dd8fa05ed66c1736f444f27c2065c1
Robsunnn <robertospasors@gmail.com>
2024-09-23 10:42:35 +0300

    Fixed the fact controller

--- src/main/java/robsunApi/web/FactController.java
+++ src/main/java/robsunApi/web/FactController.java
@@ -3,13 +3,15 @@ package robsunApi.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 import static robsunApi.service.FactService.getAFact;
 
 @RestController
 public class FactController {
 
     @GetMapping("/fact")
-    public String getFact() {
+    public HashMap<String, String> getFact() {
         return getAFact();
     }
 }
