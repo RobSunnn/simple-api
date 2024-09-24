e1a2c2a311bf8195599755ccddc28a4289354bf3
Robsunnn <robertospasors@gmail.com>
2024-09-24 11:34:02 +0300

    Changed the controller post mapping to get mapping with request param for city

--- src/main/java/robsunApi/web/WeatherController.java
+++ src/main/java/robsunApi/web/WeatherController.java
@@ -1,10 +1,7 @@
 package robsunApi.web;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import robsunApi.domain.model.CityRequest;
 
 import static robsunApi.service.WeatherService.getWeatherInfo;
@@ -12,9 +9,9 @@ import static robsunApi.service.WeatherService.getWeatherInfo;
 @RestController
 public class WeatherController {
 
-    @PostMapping("/weather")
-    public ResponseEntity<String> getWeather(@RequestBody CityRequest cityRequest) {
-        String city = cityRequest.getCity();
+    @GetMapping("/weather")
+    public ResponseEntity<String> getWeather(@RequestParam String city) {
+
         String weatherData = getWeatherInfo(city);
 
         if (weatherData != null) {
