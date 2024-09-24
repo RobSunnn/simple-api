dcbe6b540006576b5e999da4f0f58b43c9d5ff1b
Robsunnn <robertospasors@gmail.com>
2024-09-24 11:42:36 +0300

    Made the weather controller to be with a post mapping

--- src/main/java/robsunApi/web/WeatherController.java
+++ src/main/java/robsunApi/web/WeatherController.java
@@ -1,7 +1,10 @@
 package robsunApi.web;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import robsunApi.domain.model.CityRequest;
 
 import static robsunApi.service.WeatherService.getWeatherInfo;
@@ -9,9 +12,9 @@ import static robsunApi.service.WeatherService.getWeatherInfo;
 @RestController
 public class WeatherController {
 
-    @GetMapping("/weather")
-    public ResponseEntity<String> getWeather(@RequestParam String city) {
-
+    @PostMapping("/weather")
+    public ResponseEntity<String> getWeather(@RequestBody CityRequest cityRequest) {
+        String city = cityRequest.getCity();
         String weatherData = getWeatherInfo(city);
 
         if (weatherData != null) {
