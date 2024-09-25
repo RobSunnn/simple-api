700b053988b6c1f8a95e34f4f89d730c1f0ce34a
Robsunnn <robertospasors@gmail.com>
2024-09-25 17:04:21 +0300

    Added tokens to every request of the api

--- src/main/java/robsunApi/web/HobbyController.java
+++ src/main/java/robsunApi/web/HobbyController.java
@@ -1,18 +1,35 @@
 package robsunApi.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import robsunApi.service.TokenService;
 
+import java.io.Serializable;
 import java.util.HashMap;
 
+import static robsunApi.service.FactService.getAFact;
 import static robsunApi.service.HobbyGenerator.giveIdeaForHobby;
 
 @RestController
 public class HobbyController {
 
+    private final TokenService tokenService;
+
+    public HobbyController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
     @GetMapping("/hobby")
-    public HashMap<String, String> getHobby() {
-        return giveIdeaForHobby();
+    public ResponseEntity<?> getHobby(@RequestHeader("x-api-token") String token) {
+        if (tokenService.isValidToken(token)) {
+            // Proceed with request processing if token is valid
+            return ResponseEntity.ok(giveIdeaForHobby());
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
     }
 
 }
--- src/main/java/robsunApi/web/WeatherController.java
+++ src/main/java/robsunApi/web/WeatherController.java
@@ -1,26 +1,31 @@
 package robsunApi.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import robsunApi.domain.model.CityRequest;
+import robsunApi.service.TokenService;
 
+import static robsunApi.service.QuoteService.randomQuote;
 import static robsunApi.service.WeatherService.getWeatherInfo;
 
 @RestController
 public class WeatherController {
+    private final TokenService tokenService;
+
+    public WeatherController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
 
     @PostMapping("/weather")
-    public ResponseEntity<String> getWeather(@RequestBody CityRequest cityRequest) {
-        String city = cityRequest.getCity();
-        String weatherData = getWeatherInfo(city);
-
-        if (weatherData != null) {
+    public ResponseEntity<String> getWeather(@RequestHeader("x-api-token") String token, @RequestBody CityRequest cityRequest) {
+        if (tokenService.isValidToken(token)) {
+            // Proceed with request processing if token is valid
+            String city = cityRequest.getCity();
+            String weatherData = getWeatherInfo(city);
             return ResponseEntity.ok(weatherData);
         } else {
-            return ResponseEntity.status(500).body("Failed to fetch weather data");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
         }
     }
 }
