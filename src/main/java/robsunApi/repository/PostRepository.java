d734d21213c6b8f8c85609081f60d7d33ea80fc5
Robsunnn <robertospasors@gmail.com>
2024-10-02 17:38:01 +0300

    Removing the database for a while

--- src/main/java/robsunApi/repository/PostRepository.java
+++ src/main/java/robsunApi/repository/PostRepository.java
@@ -1,8 +1,8 @@
-package robsunApi.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import robsunApi.domain.entity.PostEntity;
-
-@Repository
-public interface PostRepository extends JpaRepository<PostEntity, Long> {
-}
+//package robsunApi.repository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//import robsunApi.domain.entity.PostEntity;
+//
+//@Repository
+//public interface PostRepository extends JpaRepository<PostEntity, Long> {
+//}
--- src/main/java/robsunApi/service/PostService.java
+++ src/main/java/robsunApi/service/PostService.java
@@ -1,29 +1,29 @@
-package robsunApi.service;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import robsunApi.domain.entity.PostEntity;
-import robsunApi.repository.PostRepository;
-
-import java.util.List;
-
-@Service
-public class PostService {
-
-    private final PostRepository postRepository;
-
-    @Autowired
-    public PostService(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
-
-    public List<PostEntity> getAllPosts() {
-        return postRepository.findAll();
-    }
-
-    public PostEntity createPost(PostEntity post) {
-        return postRepository.save(post);
-    }
-
-}
+//package robsunApi.service;
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import robsunApi.domain.entity.PostEntity;
+//import robsunApi.repository.PostRepository;
+//
+//import java.util.List;
+//
+//@Service
+//public class PostService {
+//
+//    private final PostRepository postRepository;
+//
+//    @Autowired
+//    public PostService(PostRepository postRepository) {
+//        this.postRepository = postRepository;
+//    }
+//
+//    public List<PostEntity> getAllPosts() {
+//        return postRepository.findAll();
+//    }
+//
+//    public PostEntity createPost(PostEntity post) {
+//        return postRepository.save(post);
+//    }
+//
+//}
