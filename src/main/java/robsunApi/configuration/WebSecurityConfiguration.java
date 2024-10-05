2285c1e0504195e8c591c0cb7c9e2916b2528fde
Robsunnn <robertospasors@gmail.com>
2024-10-05 11:24:49 +0300

    Added cors configuration and a csrf endpoint

--- src/main/java/robsunApi/configuration/WebSecurityConfiguration.java
+++ src/main/java/robsunApi/configuration/WebSecurityConfiguration.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -21,8 +27,13 @@ public class WebSecurityConfiguration {
                         authorizeRequests -> authorizeRequests
                                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                                 .requestMatchers("/", "/users/register",
-                                        "/users/login", "/users/login-error").permitAll()
+                                        "/users/login", "/users/login-error",
+                                        "/csrf-token").permitAll()
                                 .anyRequest().authenticated()
+                ).cors(
+                        cors -> cors.configurationSource(corsConfigurationSource())
+                ).csrf(
+                        csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 ).formLogin(
                         formLogin -> formLogin
                                 .loginPage("/users/login")
@@ -40,5 +51,19 @@ public class WebSecurityConfiguration {
                 );
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://robsunnn.github.io/"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("Authorization", "X-CSRF-TOKEN", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
