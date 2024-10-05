17ba601c729692b657ac52fa87132124bf97ad2e
Robsunnn <robertospasors@gmail.com>
2024-10-05 15:09:44 +0300

    Fixed a bug

--- src/main/java/robsunApi/web/rest/CsrfTokenController.java
+++ src/main/java/robsunApi/web/rest/CsrfTokenController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CsrfTokenController {
 
-    @GetMapping("")
+    @GetMapping("/csrf-token")
     public CsrfToken csrfToken(CsrfToken csrfToken) {
         return csrfToken;
     }
