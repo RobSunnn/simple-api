b5ce38858a0a1f8e78b31d960dffefa1f830a6e9
Robsunnn <robertospasors@gmail.com>
2024-10-16 16:57:14 +0300

    Removed the /remove-token endpoint in token.js

--- src/main/resources/static/js/token.js
+++ src/main/resources/static/js/token.js
@@ -18,18 +18,3 @@ document.addEventListener("DOMContentLoaded", function () {
         body: JSON.stringify({ token: token })
     }).then(response => console.log('Token stored:', response));
 });
-
-// Remove token when tab is closed
-window.addEventListener("beforeunload", function () {
-    const token = sessionStorage.getItem('apiToken');
-
-    if (token) {
-        fetch('/remove-token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ token: token })
-        }).then(response => console.log('Token removed:', response));
-    }
-});
