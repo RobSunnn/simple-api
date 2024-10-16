a8920ccdcbe3749d85e82b687f4dd77f15f06d48
Robsunnn <robertospasors@gmail.com>
2024-10-16 14:58:53 +0300

    Added validation on subscriber model, modified the token storage and some styles

--- src/main/java/robsunApi/repository/SubscriberRepository.java
+++ src/main/java/robsunApi/repository/SubscriberRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import robsunApi.domain.entity.SubscriberEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface SubscriberRepository extends JpaRepository<SubscriberEntity, Long> {
+    Optional<SubscriberEntity> findByEmail(String subscriberEmail);
+
 }
--- src/main/java/robsunApi/service/SubscriberService.java
+++ src/main/java/robsunApi/service/SubscriberService.java
@@ -1,29 +1,62 @@
 package robsunApi.service;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 import robsunApi.domain.entity.SubscriberEntity;
 import robsunApi.domain.model.binding.SubscriberBindingModel;
 import robsunApi.repository.SubscriberRepository;
 
 import java.time.LocalDate;
+import java.util.Optional;
+
+import static robsunApi.util.ResponseUtil.genericFailResponse;
+import static robsunApi.util.ResponseUtil.genericSuccessResponse;
 
 @Service
 public class SubscriberService {
 
     private final SubscriberRepository subscriberRepository;
+    private final HttpServletRequest request;
 
-    public SubscriberService(SubscriberRepository subscriberRepository) {
+    public SubscriberService(SubscriberRepository subscriberRepository, HttpServletRequest request) {
         this.subscriberRepository = subscriberRepository;
+        this.request = request;
     }
 
 
-    public boolean addSubscriber(SubscriberBindingModel subscriberBindingModel) {
+    public ResponseEntity<?> addNewSubscriber(SubscriberBindingModel subscriberBindingModel, BindingResult bindingResult) {
+        final String REDIRECT_URL = request.getHeader("referer");
 
-        subscriberRepository.save(mapToSubscriber(subscriberBindingModel));
-        return true;
+        if (bindingResult.hasErrors()) {
+            return genericFailResponse(bindingResult);
+        }
+
+        try {
+            Optional<SubscriberEntity> checkSubscriber = subscriberRepository.findByEmail(subscriberBindingModel.subscriberEmail());
+
+            if (checkSubscriber.isPresent()) {
+                SubscriberEntity subscriber = checkSubscriber.get();
+                subscriber.setSubscriptionCount(subscriber.getSubscriptionCount() + 1);
+                subscriberRepository.save(subscriber);
+
+//                if (subscriber.getCounterOfSubscriptions() == 2) {
+//                    applicationEventPublisher.publishEvent(new SendBonusVoucherEvent(
+//                            "SubscriberService", subscriber.getEmail())
+//                    );
+//                }
+                return genericSuccessResponse(REDIRECT_URL);
+            }
+
+            subscriberRepository.save(mapAsSubscriber(subscriberBindingModel));
+            return genericSuccessResponse(REDIRECT_URL);
+        } catch (Exception e) {
+            return genericFailResponse(bindingResult);
+        }
     }
 
-    private SubscriberEntity mapToSubscriber(SubscriberBindingModel subscriberBindingModel) {
+    private SubscriberEntity mapAsSubscriber(SubscriberBindingModel subscriberBindingModel) {
         return new SubscriberEntity()
                 .setEmail(subscriberBindingModel.subscriberEmail())
                 .setSubscriptionTime(LocalDate.now())
